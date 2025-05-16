7fba8b4643fe06de3fa6086ddec0094d8bdefff7
Abrahan <abrahanramirez2410@gmail.com>
2025-05-15 20:51:29 -0400

    agregando fondo

--- 3 en linea/src/App.tsx
+++ 3 en linea/src/App.tsx
@@ -2,7 +2,19 @@ import "./App.css";
 import Tablero from "./components/Tablero";
 
 function App() {
-  return <Tablero />;
+  return (
+    <div
+      style={{
+        backgroundColor: "#1B1E23",
+        width: "100vw",
+        height: "100vh",
+        minHeight: "100vh",
+        minWidth: "100vw",
+      }}
+    >
+      <Tablero />
+    </div>
+  );
 }
 
 export default App;
